e2eddfb711c99327456988187ceedf71f7b6f71c
rahulpateriya <rahulpateriya287@gmai.com>
2025-07-10 23:38:14 +0530

    test msg write here

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ resource "azurerm_resource_group" "rg1" {
   location = "east us"
 }
 
-resource "azurerm_resource_group" "rg21" {
-  name = "ram2"
-  location = "east us"
-}
+# sare jaha se achha hindustan hamaara
+# abcd
+# xyz
+# maneesh bhai
