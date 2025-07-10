a16c46210f989512ebf17a7b791bec413be86961
unknown <bhawani.bid@gmail.com>
2025-07-10 22:58:42 +0530

    one resource add

---
+++ main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.35.0"
+    }
+  }
+}
+
+provider "azurerm" {
+ features {}
+ subscription_id = "92e22e38-2f32-450c-97de-3c896645b2da"
+}
+
+resource "azurerm_resource_group" "rg1" {
+  name = "ram"
+  location = "east us"
+}
